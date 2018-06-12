4d8d65d44491a629c90d40169e32407e00bd7e02
Ben Meadows <benmeadows@Bens-MacBook-Pro.local>
2018-06-12 12:09:39 -0500

    Adding the final base files and modifications to the project

--- cucumbertests/src/test/groovy/steps/geb_steps.groovy
+++ cucumbertests/src/test/groovy/steps/geb_steps.groovy
@@ -1,6 +1,20 @@
 import page.*
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import apiutils.*;
 
 import static cucumber.api.groovy.EN.*
+
+def response
 
 Given(~/I am on the "Grunt Home" page/) { ->
 	to GruntHomePage
@@ -17,3 +31,25 @@ Then(~/I am taken to the "How It Works" page/) { ->
 Then(~/My branded icon should have the "Guntwork.io" text/) { ->
 	page.verifyPageHeading()
 }
+
+//HTTP Steps. Steps for API tests should never be kept in the same project as 
+
+When(~/I ping the Hello World endpoint/) { ->
+	def apiUtils = new APIUtilities("http://localhost:8080/hello/world")
+    response = apiUtils.returnResponseFromGetAsString()      
+}
+
+When(~/I ping the base URL/) { ->
+    def apiUtils = new APIUtilities("http://localhost:8080/")
+    response = apiUtils.returnResponseFromGetAsString()                       
+                       }
+
+Then(~/I get the response "hello : world"/)  { ->
+    assert response == "{\"hello\":\"world\"}" 
+}
+
+Then(~/I get a response that has the defaultUser's info/) { ->
+   assert response.contains("Easy St.")                                         
+                                                   }
+
+
